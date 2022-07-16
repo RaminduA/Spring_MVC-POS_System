0dcbdfdf08a0af522d3861b9f8da1244d2777037
RaminduA <raminduanjana22171@gmail.com>
2022-07-16 14:52:46 +0530

    Added Application Properties

--- Backend/src/main/java/com/scorpion/spring/config/JpaConfig.java
+++ Backend/src/main/java/com/scorpion/spring/config/JpaConfig.java
@@ -1,14 +1,19 @@
 package com.scorpion.spring.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.transaction.PlatformTransactionManager;
-import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.vendor.Database;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 
 import javax.sql.DataSource;
 import javax.persistence.EntityManagerFactory;
@@ -16,7 +21,10 @@ import javax.persistence.EntityManagerFactory;
 @Configuration
 @EnableJpaRepositories(basePackages = "com.scorpion.spring.repository")
 @EnableTransactionManagement
+@PropertySource("classpath:application.properties")
 public class JpaConfig {
+    @Autowired
+    Environment environment;
 
     @Bean
     public PlatformTransactionManager transactionManager(EntityManagerFactory factory){
@@ -28,17 +36,27 @@ public class JpaConfig {
         LocalContainerEntityManagerFactoryBean bean = new LocalContainerEntityManagerFactoryBean();
         bean.setJpaVendorAdapter(vendor);
         bean.setDataSource(source);
-        bean.setPackagesToScan("com.scorpion.spring.entity");
+        bean.setPackagesToScan(environment.getRequiredProperty("app.entity.package"));
         return bean;
     }
 
     @Bean
     public DataSource dataSource(){
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
-        dataSource.setUrl("jdbc:mysql://localhost:3306/SpringMavenCompany?createDatabaseIfNotExist=true");
-        dataSource.setUsername("root");
-        dataSource.setPassword("1234");
-        dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+        dataSource.setUrl(environment.getProperty("app.db.url"));
+        dataSource.setUsername(environment.getRequiredProperty("app.db.username"));
+        dataSource.setPassword(environment.getRequiredProperty("app.db.password"));
+        dataSource.setDriverClassName(environment.getRequiredProperty("app.driver.classname"));
         return dataSource;
+    }
+
+    @Bean
+    public JpaVendorAdapter jpaVendorAdapter(){
+        HibernateJpaVendorAdapter vendor = new HibernateJpaVendorAdapter();
+        vendor.setDatabasePlatform(environment.getRequiredProperty("app.db.platform"));
+        vendor.setDatabase(Database.MYSQL);
+        vendor.setShowSql(true);
+        vendor.setGenerateDdl(true);
+        return vendor;
     }
 }
