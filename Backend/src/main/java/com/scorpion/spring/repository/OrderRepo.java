c7080e753780f5a3ea5f76ab79f0f3bb28c32319
RaminduA <raminduanjana22171@gmail.com>
2022-07-19 18:37:29 +0530

    Updated Repositories

--- Backend/src/main/java/com/scorpion/spring/repository/OrderRepo.java
+++ Backend/src/main/java/com/scorpion/spring/repository/OrderRepo.java
@@ -4,5 +4,5 @@ import com.scorpion.spring.entity.Orders;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface OrderRepo extends JpaRepository<Orders,String> {
-
+    Orders findTopByOrderByIdDesc();
 }
